beab3393d97da5d151854e16aef0c32d65cedaa5
Faruk <faruk@alphacommerz.com>
2023-11-26 20:19:45 +0600

    feat: DOM mastering with project now working

---
+++ Finished/1. What Is DOM/app.js
@@ -0,0 +1,11 @@
+// console.log(document);
+
+// console.log(document.querySelectorAll('input'))
+
+// console.log(document);
+// console.log(document.head);
+// console.log(document.title);
+// console.log(document.body);
+// console.log(document.documentURI);
+// console.log(document);
+ 
